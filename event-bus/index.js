4afc297a624661cff0261308455ee8159d6b99f8
Tatiana Grigorieva <tsg1204@gmail.com>
2020-05-01 12:12:41 -0400

    events tested

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/post/events', (req, res) => {
+app.post('/events', (req, res) => {
     const event = req.body;
 
     axios.post('http://localhost:4000/events', event);
